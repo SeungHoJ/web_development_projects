4158e7a5c8da4b0b781b9ed823672d423afcd6ec
seungho <tkdgus7993@naver.com>
2025-04-19 15:29:59 +0900

    feat : react-ize project finished

--- 10000hoursrule/src/App2.jsx
+++ 10000hoursrule/src/App2.jsx
@@ -48,30 +48,22 @@ function App() {
       minHeight: '100vh', 
       fontFamily: 'GmarketSansMedium'}}>
      <Container maxWidth='md' sx={{pt:'5'}}>
-      <Box sx={{display : 'flex', justifyContent : 'center', alignItems : 'center', mb : 5}}>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems : 'center'
+      }
+      }>
+        <img src={ClockImage} alt="Clock" width={265} height={265} style={{
+          position: 'relative'
+        }} />
         <Box sx={{
-          position : 'relative',
-          width : 265,
-          height : 265,
-          display : 'flex',
-          justifyContent : 'center',
-          alignContent : 'center'
+          position : 'absolute',
+          width: 564
         }}>
-          <img src={ClockImage} alt="clock" width={265} height={265} style={{position : 'absolute', top : 0, left : 0}} />
-          <Box sx={{
-            position : 'absolute',
-            top : '50%',
-            left : '50%',
-            transform : 'translate(-50%, -50%)',
-            width : 600
-          }}>
-            <img src={TitleImage} alt="title" style={{width : '100%'}} />
-
-          </Box>
-
+          <img src={TitleImage} alt="title" style={{width : '100%'}} />
         </Box>
       </Box>
-      
       <InputForm 
         field={field}
         setField={setField}
--- 10000hoursrule/src/components/ResultBox.jsx
+++ 10000hoursrule/src/components/ResultBox.jsx
@@ -16,6 +16,20 @@ const ResultBox = ({loading, resultVisible, field, time, onModalOpen, onShare })
           }} />
         </Box>
       )}
+
+      {resultVisible && (
+        <Box sx={{display : 'flex', flexDirection : 'column', alignItems : 'center', lineHeight : '1.5', mb : 8}}>
+          <Typography sx={{fontSize : 20}}>당신은 <span style={{fontSize : 70}}>{field}</span> 전문가가 되기 위해서</Typography>
+          <Typography sx={{fontSize : 20}}>대략 <span style={{fontSize : 70}}>{calculatedDays}</span>일 이상 훈련하셔야 합니다 !!</Typography>
+          <Box sx={{display : 'flex', justifyContent : 'center', mt : 7}}>
+            <Button variant="contained" onClick={onModalOpen} sx={{backgroundColor : '#fcee21', color : '#5b2386'}}>훈련하러 가기 
+            </Button>
+            <Button variant="outlined" onClick={onShare} sx={{backgroundColor : '#fff', color : '#5b2386'}}>
+            Share
+            </Button>
+          </Box>
+        </Box>
+      )}
     </>
   )
 }
